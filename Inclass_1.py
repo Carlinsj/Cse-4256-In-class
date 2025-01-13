9b574e9f826aa9fdb5d8197e882fe00970d8c521
Carlinsj <carlinsjoseph17@gmail.com>
2025-01-13 18:12:50 -0500

    Test cases removed

--- Inclass_1.py
+++ Inclass_1.py
@@ -19,13 +19,9 @@ def sum_largest(li):
         elif num > second_largest:
             second_largest = num
     return largest + second_largest
-example_list = [10, 4, 6, 12, -3, 1, 14, -7, 9]
-print(sum_largest(example_list)) 
+
 end_time = time.time()
-
 elapsed_time = end_time - start_time
-
-print("Elapsed time:", elapsed_time)
 
 # Question 3
 import time
@@ -34,16 +30,13 @@ start_time = time.time()
 def sum_largest(li):
     sorted_list = sorted(li)
     return sorted_list[-1] + sorted_list[-2]
-example_list = [10, 4, 6, 12, -3, 1, 14, -7, 9]
-print(sum_largest(example_list))  
 end_time = time.time()
 
 elapsed_time = end_time - start_time
 
-print("Elapsed time:", elapsed_time)
 
 #Question 4(a)
-#Function 2 will be faster since it will take O(n) time will function 3 takes (nlog(n))
+#Function 2 will be faster since it will take O(n) time while function 3 takes O(nlog(n))
 
 #Question 4(b)
 #In Python, the key difference between the sort() method and the sorted() function is that 
@@ -70,11 +63,6 @@ def merge(li1, li2):
         j += 1
     return merged_list
 
-# Example usage
-list1 = [2, 4, 5, 8, 10, 11, 13]
-list2 = [3, 6, 7, 8, 10, 12]
-print(merge(list1, list2))  
-
 #Question 6
 def largest_so_far(li):
     # Initialize variables
@@ -88,8 +76,7 @@ def largest_so_far(li):
             max_so_far = num 
     
     return count
-example_list = [10, 8, 9, 9, 13, 16, 12]
-print(largest_so_far(example_list))  
+
 
 #Question 7
 def evens(li):
@@ -99,8 +86,7 @@ def evens(li):
         if l[num]%2==0:
             count+=1
     return count
-l = [3, -2, 4, 7, -6, 7, 5]
-print(evens(l))
+
 
 #Question 8
 def linear_search(li, num):
@@ -108,9 +94,7 @@ def linear_search(li, num):
         if element == num:
             return True  
     return False
-example_list = [3, -2, 4, 7, -6, 7, 5]
-print(linear_search(example_list, 4))  
-print(linear_search(example_list, 10))
+
 
 # Question 9
 def binary_search(li, num):
@@ -125,9 +109,7 @@ def binary_search(li, num):
         else:
             low = mid + 1
     return False
-ordered_list = [1, 3, 5, 7, 9, 11, 13, 15]
-print(binary_search(ordered_list, 15))  # Output: True
-print(binary_search(ordered_list, 8))  # Output: False
+
 
 #Question 10
 #Worst Case for linear search O(n)
